6ac2639c55812bed8d3a192a31864449ffa57949
nicolequindara <cquindara@gmail.com>
2018-02-17 18:13:38 -0700

    Add project files.

---
+++ CalculatorConsole/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsole
+{
+    class Program
+    {
+        enum OPERATION
+        {
+            ADD = 1,
+            SUBTRACT = 2,
+            MULTIPLY = 3,
+            DIVIDE = 4
+        }
+
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                try
+                {
+                    String input;
+                    int a, b;
+                    int type;
+
+                    write("Enter first integer argument:\t ");
+                    input = Console.ReadLine();
+                    int.TryParse(input, out a);
+
+
+                    write("Enter second integer argument:\t ");
+                    input = Console.ReadLine();
+                    int.TryParse(input, out b);
+
+                    write("Enter type of operation:\n");
+                    write("\t 1: ADD\n");
+                    write("\t 2: SUBTRACT\n");
+                    write("\t 3: MULTIPLY\n");
+                    write("\t 4: DIVIDE\n");
+                    input = Console.ReadLine();
+                    int.TryParse(input, out type);
+
+                    Calculate(a, b, (OPERATION)type);
+
+                }
+                catch(Exception ex)
+                {
+                    printError(ex.Message);
+                }
+            }
+        }
+
+        static void Calculate(int a, int b, OPERATION type)
+        {
+            localhost.CalculatorService calculatorService = new localhost.CalculatorService();
+            int result;
+
+            switch (type)
+            {
+                case OPERATION.ADD:
+                    result = calculatorService.Add(a, b);
+                    printSuccess(string.Format("{0} + {1} = {2}", a, b, result));
+                    break;
+                case OPERATION.SUBTRACT:
+                    result = calculatorService.Subtract(a, b);
+                    printSuccess(string.Format("{0} - {1} = {2}", a, b, result));
+                    break;
+                case OPERATION.MULTIPLY:
+                    result = calculatorService.Multiply(a, b);
+                    printSuccess(string.Format("{0} * {1} = {2}", a, b, result));
+                    break;
+                case OPERATION.DIVIDE:
+                    result = calculatorService.Divide(a, b);
+                    printSuccess(string.Format("{0} / {1} = {2}", a, b, result));
+
+                    break;
+                default:
+                    printError("Unknown operation: " + type);
+                    break;
+            }
+        }
+
+        static void write(string message)
+        {
+            Console.Write(message);
+        }
+
+        static void printError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void printSuccess(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ SoapSample/CalculatorService.asmx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SoapSample
+{
+    /// <summary>
+    /// Summary description for CalculatorService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorService : System.Web.Services.WebService
+    {
+
+        [WebMethod(Description  = "Adds two integers")]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        [WebMethod(Description = "Subtracts the second integer argument from the first")]
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        [WebMethod(Description = "Multiplies two integers")]
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        [WebMethod(Description = "Adds two integers")]
+        public int Divide(int a, int b)
+        {
+            if (b == 0) return -1;
+            return a / b;
+        }
+    }
+}
